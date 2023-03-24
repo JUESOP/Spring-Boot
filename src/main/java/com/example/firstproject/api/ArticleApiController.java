efc0025aa760606a1174909c58a1d972d9d62099
JUESOP <sinangun@naver.com>
2023-03-24 10:41:06 +0900

    ArticleService

--- src/main/java/com/example/firstproject/api/ArticleApiController.java
+++ src/main/java/com/example/firstproject/api/ArticleApiController.java
@@ -3,6 +3,8 @@ package com.example.firstproject.api;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import com.example.firstproject.repository.ArticleRepository;
+import com.example.firstproject.service.ArticleService;
+import com.fasterxml.jackson.databind.deser.std.ObjectArrayDeserializer;
 import org.aspectj.weaver.patterns.HasMemberTypePatternForPerThisMatching;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,63 +16,63 @@ import java.util.List;
 @RestController //데이터(JSON)를 반환
 public class ArticleApiController {
 
-    @Autowired
-    private ArticleRepository articleRepository;
+    @Autowired //DI, 생성 객체를 가져와 연결!
+    private ArticleService articleService;
 
     //GET
     @GetMapping("/api/articles")
     public List<Article> index() {
-        return articleRepository.findAll();
+        return articleService.index();
     }
 
     @GetMapping("/api/articles/{id}")
-    public Article index(@PathVariable Long id) {
-        return articleRepository.findById(id).orElse(null);
+    public Article show(@PathVariable Long id) {
+        return articleService.show(id);
     }
 
     //POST
     @PostMapping("/api/articles")
-    public Article create(@RequestBody ArticleForm dto) {
-        Article article = dto.toEntity();
-        return articleRepository.save(article);
+    public ResponseEntity<Article> create(@RequestBody ArticleForm dto) {
+        Article created = articleService.create(dto);
+        return (created != null) ? ResponseEntity.status(HttpStatus.OK).body(created) : ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
     }
 
-    //PATCH
-    @PatchMapping("/api/articles/{id}")
-    public ResponseEntity<Article> update(@PathVariable Long id,
-                                          @RequestBody ArticleForm dto) {
-        // 1.수정용 엔티티 생성
-        Article article = dto.toEntity();
-
-        // 2. 대상 엔티티를 조회
-        Article target = articleRepository.findById(id).orElse(null);
-
-        // 3. 잘못된 요청 처리(대상이 없거나, id가 다른 경우)
-        if (target == null || id != article.getId()) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-        }
-
-        // 4. 업데이트 및 정상 응답(200)
-        target.patch(article);
-        Article updated = articleRepository.save(target);
-        return ResponseEntity.status(HttpStatus.OK).body(updated);
-    }
-
-    //DELETE
-    @DeleteMapping("/api/articles/{id}")
-    public ResponseEntity<Article> delete(@PathVariable Long id) {
-
-        //대상 찾기
-        Article target = articleRepository.findById(id).orElse(null);
-
-        //잘못된 요청 처리
-        if (target == null) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
-        }
-
-        //대상 삭제
-        articleRepository.delete(target);
-        return ResponseEntity.status(HttpStatus.OK).build();
-    }
+//    //PATCH
+//    @PatchMapping("/api/articles/{id}")
+//    public ResponseEntity<Article> update(@PathVariable Long id,
+//                                          @RequestBody ArticleForm dto) {
+//        // 1.수정용 엔티티 생성
+//        Article article = dto.toEntity();
+//
+//        // 2. 대상 엔티티를 조회
+//        Article target = articleRepository.findById(id).orElse(null);
+//
+//        // 3. 잘못된 요청 처리(대상이 없거나, id가 다른 경우)
+//        if (target == null || id != article.getId()) {
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+//        }
+//
+//        // 4. 업데이트 및 정상 응답(200)
+//        target.patch(article);
+//        Article updated = articleRepository.save(target);
+//        return ResponseEntity.status(HttpStatus.OK).body(updated);
+//    }
+//
+//    //DELETE
+//    @DeleteMapping("/api/articles/{id}")
+//    public ResponseEntity<Article> delete(@PathVariable Long id) {
+//
+//        //대상 찾기
+//        Article target = articleRepository.findById(id).orElse(null);
+//
+//        //잘못된 요청 처리
+//        if (target == null) {
+//            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
+//        }
+//
+//        //대상 삭제
+//        articleRepository.delete(target);
+//        return ResponseEntity.status(HttpStatus.OK).build();
+//    }
 
 }
---
+++ src/main/java/com/example/firstproject/service/ArticleService.java
@@ -0,0 +1,31 @@
+package com.example.firstproject.service;
+
+import com.example.firstproject.dto.ArticleForm;
+import com.example.firstproject.entity.Article;
+import com.example.firstproject.repository.ArticleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service //서비스 선언! (서비스 객체를 스프링부트에 생성)
+public class ArticleService {
+    @Autowired //DI
+    private ArticleRepository articleRepository;
+
+    public List<Article> index() {
+        return articleRepository.findAll();
+    }
+
+    public Article show(Long id) {
+        return articleRepository.findById(id).orElse(null);
+    }
+
+    public Article create(ArticleForm dto) {
+        Article article = dto.toEntity();
+        if (article.getId() != null) {
+            return null;
+        }
+        return articleRepository.save(article);
+    }
+}
